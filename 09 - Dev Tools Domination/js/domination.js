f3b25137a68fd251044879c6b5c37e99d45eaec7
jeohong <jeohong59@gmail.com>
2021-12-31 11:48:20 +0900

    Dev Tools Domination complate

--- 09 - Dev Tools Domination/js/domination.js
+++ 09 - Dev Tools Domination/js/domination.js
@@ -10,25 +10,70 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('Hello I am a %s string!', '💩');
 
 // Styled
+console.log(
+  '%c I am some great text',
+  'font-size:50px; background:red; text-shadow: 10px 10px 0 blue'
+);
 
 // warning!
+console.warn('NOOO');
 
 // Error :|
+console.error('Err~!!!');
 
 // Info
+console.info('informaion');
 
 // Testing
+const p = document.querySelector('p');
+console.assert(
+  p.classList.contains('hello'),
+  'Not found class name is [hello]'
+);
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.dir(p);
+console.clear();
 
 // Grouping together
+dogs.forEach((dog) => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then((data) => data.json())
+  .then((data) => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs);
