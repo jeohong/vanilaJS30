ce1f571c4718160c6c8e01a84e009ca853fd6091
jeohong <jeohong59@gmail.com>
2021-12-31 14:09:27 +0900

    Slide in on Scroll complate

--- 13 - Slide in on Scroll/js/script.js
+++ 13 - Slide in on Scroll/js/script.js
@@ -13,3 +13,21 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImgs = document.querySelectorAll('.slide-in');
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+function checkSlide() {
+  sliderImgs.forEach((sliderImg) => {
+    const sliderInAt = window.scrollY + window.innerHeight - sliderImg.height / 2;
+    const imageBottom = sliderImg.offsetTop + sliderImg.height;
+    const isHalfShown = sliderInAt > sliderImg.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImg.classList.add('active');
+    } else {
+      sliderImg.classList.remove('active');
+    }
+  });
+}
